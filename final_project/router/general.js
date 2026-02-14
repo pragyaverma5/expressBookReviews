30bf5207a5e4a928909f5cfef6f1db8938d41542
Pragya Verma <162286680+pragyaverma5@users.noreply.github.com>
2026-02-14 22:52:26 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,9 +19,10 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const isbn = req.params.isbn;
+  return res.status(200).json(books[isbn]);
+});
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
