4d050982028857f821c847d93879d5b5e1f3ed00
Pragya Verma <162286680+pragyaverma5@users.noreply.github.com>
2026-02-14 23:07:36 +0530

    Add user registration and book title filtering
    Implemented user registration and book filtering by title.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,9 +6,21 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(400).json({message: "Username and password required"});
+  }
+
+  if (users.find(user => user.username === username)) {
+    return res.status(409).json({message: "User already exists"});
+  }
+
+  users.push({username: username, password: password});
+  return res.status(200).json({message: "User successfully registered"});
 });
+
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
@@ -38,14 +50,21 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title.toLowerCase();
+
+  const filteredBooks = Object.values(books).filter(book =>
+    book.title.toLowerCase() === title
+  );
+
+  return res.status(200).json(filteredBooks);
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.status(200).json(books[isbn].reviews);
 });
+
 
 module.exports.general = public_users;
