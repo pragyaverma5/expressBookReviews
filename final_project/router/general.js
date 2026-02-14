f66b23a71e382af3c0be836be6b507af763aeeed
Pragya Verma <162286680+pragyaverma5@users.noreply.github.com>
2026-02-14 22:55:45 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,9 +26,15 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author.toLowerCase();
+
+  const filteredBooks = Object.values(books).filter(book =>
+    book.author.toLowerCase() === author
+  );
+
+  return res.status(200).json(filteredBooks);
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
